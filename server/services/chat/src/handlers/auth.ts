f66d1bb2307175afe70f686f83d6604ad356e8ba
zaraser <123326731+zaraser@users.noreply.github.com>
2026-01-07 22:56:39 +0100

    Add files via upload

---
+++ server/services/chat/src/handlers/auth.ts
@@ -0,0 +1,22 @@
+import { unregisterSocket, getAllUsers } from "../socketRegistry.js";
+
+export function registerAuthHandlers(io: any, socket: any) {
+  if (socket.user) {
+    io.emit("users:online", getAllUsers());
+  }
+
+  socket.on("logout", () => {
+    if (!socket.user) {
+      socket.disconnect(true);
+      return;
+    }
+
+    const wentOffline = unregisterSocket(socket.user.id, socket.id);
+
+    if (wentOffline) {
+      io.emit("users:online", getAllUsers());
+    }
+
+    socket.disconnect(true);
+  });
+}
